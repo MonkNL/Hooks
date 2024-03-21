b00c3b3a21eee1c978d1a0c96a453979fef2c925
MonkNL <info@thomasvv.nl>
2024-03-21 10:08:26 +0100

    Create helper_functions.php

---
+++ src/helper_functions.php
@@ -0,0 +1,10 @@
+<?php 
+
+function do_hook(string $hook,... $args):void{
+	Hooks::do_hook($hook,$args);
+}
+function add_action(string $hook,callable $callback,int $priority = 10,int $acceptedArguments  = 0):void{
+	Hooks::add_action($hook,$callback,$priority,$acceptedArguments);
+}
+
+?>
